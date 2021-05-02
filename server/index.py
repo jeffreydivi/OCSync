4324977595e53d487b68dc38c033a6dab62b3e21
Jeffrey <43482496+jeffreydivi@users.noreply.github.com>
2021-05-01 23:00:52 -0400

    Runtime optimizations (@Kurtoid), basic auth.

--- server/index.py
+++ server/index.py
@@ -15,47 +15,42 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = config['secret_key']
 socketio = SocketIO(app)
 
+# This is where we'll store data.
+memory = {}
+
 @app.route('/')
 def index():
-    return "hello there"
+    return "<h1>Coming Soon</h1>"
 
-# From https://www.geeksforgeeks.org/python-program-for-binary-search/
-def binSearchDict(arr, name):
-    high = len(arr)
-    low = 0
-    mid = (high + low) // 2
-
-    if high >= low:
-        if arr[mid]["name"] < name:
-            low = mid + 1
-        # If x is smaller, ignore right half
-        elif arr[mid]["name"] > name:
-            high = mid - 1
-        # means x is present at mid
+@app.route('/api/data')
+def data():
+    try:
+        creds = request.authorization
+        if validateKey(creds["username"], creds["password"]):
+            return Response(json.dumps(memory), mimetype="application/json")
         else:
-            return mid
-    return -1
+            return createError(403, "Incorrect credentials.")
+    except:
+        return createError(401, "Please set Authorization header.")
+
+def createError(code, message):
+    return Response(json.dumps({"error": code, "reason": message}), mimetype="application/json", status=code)
 
 def validateKey(name, password):
     current_db = config["keys"]
 
-    idx = binSearchDict(current_db, name)
-    if (idx == -1):
+    try:
+        hashed = config["keys"][name]
+    except:
         return False
 
-    hashed = config["keys"][idx]["key"]
-
     return bcrypt.checkpw(password.encode(encoding="ascii"), base64.b64decode(hashed))
-    # hashed = name in config["keys"]
 
 def addKey(name, key):
     # Hash password and add to the "database"
     password = base64.b64encode(bcrypt.hashpw(key.encode(encoding="ascii"), bcrypt.gensalt())).decode()
     current_db = config["keys"]
-    current_db.append({"name": name, "key": password})
-
-    # Sort the DB so we can do O(log n) search.
-    current_db = sorted(current_db, key=lambda item: item['name'])
+    current_db[name] = password
 
     # Update the file
     config["keys"] = current_db
@@ -64,4 +59,5 @@ def addKey(name, key):
         config_file.write(json.dumps(config))
 
 if __name__ == '__main__':
+    addKey("cat", "uwsu")
     socketio.run(app, host="0.0.0.0", port=config["port"], debug=config["debug"])
